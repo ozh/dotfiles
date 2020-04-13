5479ab7cfb443e8ab7ee553eb9e4058ce04de01b
Ozh <ozh@ozh.org>
2020-04-13 15:52:02 +0200

    Typos

--- local.sh
+++ local.sh
@@ -4,7 +4,7 @@ cd ~
 mkdir -p ~/bin
 
 # Install precompiled nano 4.9.2 and highlight files
-[ ! -f ~/bin/nano ] && wget -O ~/bin/nano https://ozh.org/nano/nano && chmod + ~/bin/nano
+[ ! -f ~/bin/nano ] && wget -O ~/bin/nano https://ozh.org/nano/nano && chmod +x ~/bin/nano
 [ ! -d ~/.nano/ ]   && curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
 
 # Install my binaries
