40a219639cd6f52c3369b7dd75a5e8ffa04d3b74
྅༻ Ǭɀħ ༄༆ཉ <ozh@ozh.org>
2022-07-13 15:04:18 +0200

    Update nano & bat if newer available

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 cd ~
 mkdir -p ~/bin
 
-# Install precompiled nano 4.9.2 and highlight files
-[ ! -f ~/bin/nano ] && wget -O ~/bin/nano https://ozh.org/nano/nano && chmod +x ~/bin/nano
-[ ! -d ~/.nano/ ]   && curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
+# Install compiled nano 6.3 if missing or older, and highlight files
+[[ ! -f  ~/bin/nano || $(nano -V | grep version | sed 's/^[^0-9.-]*//') != '6.3' ]] && rm -f ~/bin/nano && wget -O ~/bin/nano https://ozh.org/nano/nano && chmod +x ~/bin/nano
+[[ ! -d ~/.nano/ ]] && curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
 
-# Install precompiled bat 0.22.0 
-# When there is new binaries: https://github.com/sharkdp/bat/releases look for bat-vX.XX.X-x86_64-unknown-linux-musl.tar.gz
-[ ! -f ~/bin/bat ] && wget -O ~/bin/bat https://ozh.org/bat/bat && chmod +x ~/bin/bat
+# Install precompiled bat 0.21.0 if missing or older
+# Check for new binaries: https://github.com/sharkdp/bat/releases look for bat-vX.XX.X-x86_64-unknown-linux-musl.tar.gz
+[[ ! -f ~/bin/bat || $(bat --version | sed 's/^[^0-9.-]*//' | cut -d\  -f1) != "0.21.0" ]] && rm -f ~/bin/bat && wget -O ~/bin/bat https://ozh.org/bat/bat && chmod +x ~/bin/bat
 
 # if we have a .nanorc, add tabsize
 if [ -f ~/.nanorc ] && ! grep -q "set tabsize 4" ~/.nanorc ; then
